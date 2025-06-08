657b21a5ef7a690845a21f6d8f82463b36582c0d
Ben Everly <ben.everly@oberd.com>
2025-06-08 10:37:55 -0500

    init tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ["@/assets/css/main.css"],
+});
