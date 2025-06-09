bc595d6ddc22009c3d71138e9dcc765b3f02d02c
Ben Everly <ben.everly@oberd.com>
2025-06-09 15:34:24 -0500

    Replace `<NuxtWelcome />` with `<NuxtPage />` and add sample content

---
+++ pages/article/[slug].vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex flex-col items-center">
+    <NuxtLink class="flex" to="/">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6 mb-4"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path d="M12 19l-7-7 7-7" />
+      </svg>
+      Home
+    </NuxtLink>
+    <h1>Article Title</h1>
+    <dl class="flex gap-2">
+      <dt>Date:</dt>
+      <dd>
+        <NuxtTime
+          datetime="2025-06-09T17:26:00Z"
+          relative
+          numeric="auto"
+          style="long"
+        />
+      </dd>
+    </dl>
+    <p>This is the article page.</p>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col items-center">
+    <h1>Ben Everly</h1>
+    <p>Hi, I'm Ben. This is my website. I'm a software engineer with a passion for building scalable web applications.</p>
+    <ol class="list-none">
+      <li>
+        <NuxtLink to="/article/first-article">First Article</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/article/second-article">Second Article</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/article/third-article">Third Article</NuxtLink>
+      </li>
+    </ol>
+  </div>
+</template>
