a51a3bb7d11b1f234d0bcdfd57d5bd4e9828a0e2
Ben Everly <ben.everly@oberd.com>
2025-06-09 22:23:23 -0500

    Add custom domain, ssl

--- infra/index.ts
+++ infra/index.ts
@@ -34,9 +34,46 @@ const bucketFolder = new synced_folder.S3BucketFolder("bucket-folder", {
     acl: "public-read",
 }, { dependsOn: [ownershipControls, publicAccessBlock]});
 
+const eastRegion = new aws.Provider("east", {
+    profile: aws.config.profile,
+    region: "us-east-1",
+});
+
+const certificateConfig: aws.acm.CertificateArgs = {
+    domainName: "beneverly.com",
+    validationMethod: "DNS",
+    subjectAlternativeNames: [`www.beneverly.com`],
+};
+
+const certificate = new aws.acm.Certificate("certificate", certificateConfig, { provider: eastRegion });
+
+const zoneId = aws.route53.getZone({ name: 'beneverly.com' }, { async: true }).then(zone => zone.zoneId);
+
+const certificateValidationDomain = new aws.route53.Record(`beneverly.com-validation`, {
+    name: certificate.domainValidationOptions[0].resourceRecordName,
+    zoneId: zoneId,
+    type: certificate.domainValidationOptions[0].resourceRecordType,
+    records: [certificate.domainValidationOptions[0].resourceRecordValue],
+    ttl: 600,
+});
+
+const subdomainCertificateValidationDomain = new aws.route53.Record(`beneverly.com-www-validation`, {
+    name: certificate.domainValidationOptions[1].resourceRecordName,
+    zoneId: zoneId,
+    type: certificate.domainValidationOptions[1].resourceRecordType,
+    records: [certificate.domainValidationOptions[1].resourceRecordValue],
+    ttl: 600,
+});
+
+const certificateValidation = new aws.acm.CertificateValidation("certificateValidation", {
+    certificateArn: certificate.arn,
+    validationRecordFqdns: [certificateValidationDomain.fqdn, subdomainCertificateValidationDomain.fqdn],
+}, { provider: eastRegion });
+
 // Create a CloudFront CDN to distribute and cache the website.
 const cdn = new aws.cloudfront.Distribution("cdn", {
     enabled: true,
+    aliases: ["beneverly.com", "www.beneverly.com"],
     origins: [{
         originId: bucket.arn,
         domainName: bucketWebsite.websiteEndpoint,
@@ -82,9 +119,42 @@ const cdn = new aws.cloudfront.Distribution("cdn", {
         },
     },
     viewerCertificate: {
-        cloudfrontDefaultCertificate: true,
+        acmCertificateArn: certificateValidation.certificateArn,
+        sslSupportMethod: "sni-only",
     },
 });
+
+// Creates a new Route53 DNS record pointing the domain to the CloudFront distribution.
+new aws.route53.Record(
+    "beneverly.com",
+    {
+        name: '',
+        zoneId: zoneId,
+        type: "A",
+        aliases: [
+            {
+                name: cdn.domainName,
+                zoneId: cdn.hostedZoneId,
+                evaluateTargetHealth: true,
+            },
+        ],
+    }
+);
+new aws.route53.Record(
+    "beneverly.com-www-alias",
+    {
+        name: 'www.beneverly.com',
+        zoneId: zoneId,
+        type: "A",
+        aliases: [
+            {
+                name: cdn.domainName,
+                zoneId: cdn.hostedZoneId,
+                evaluateTargetHealth: true,
+            },
+        ],
+    }
+)
 
 // Export the URLs and hostnames of the bucket and distribution.
 export const originURL = pulumi.interpolate`http://${bucketWebsite.websiteEndpoint}`;
