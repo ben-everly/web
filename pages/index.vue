3cb54bd0226806eace55f8de73c98d832c8d578f
Ben Everly <ben.everly@oberd.com>
2025-06-11 20:40:42 -0500

    @nuxt/icon

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,12 @@ const { data: articles } = await useAsyncData(() =>
 <template>
   <div class="flex flex-col items-center">
     <h1>Ben Everly</h1>
+    <NuxtLink
+      to="https://github.com/ben-everly"
+      class="inline-flex items-center"
+    >
+      <Icon name="uil:github" class="bg-black size-5" />ben-everly
+    </NuxtLink>
     <p>
       Hi, I'm Ben. This is my website. I'm a software engineer with a passion
       for building scalable web applications.
