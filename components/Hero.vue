e0942a4cf9bea1a3bbdca247f214004df78e2f98
Ben Everly <ben.everly@oberd.com>
2025-07-02 23:58:11 -0500

    make the grid pattern more visible

--- components/Hero.vue
+++ components/Hero.vue
@@ -2,7 +2,7 @@
     <header
         class="from-primary-600 via-primary-700 to-primary-800 animated-gradient relative overflow-hidden bg-gradient-to-br px-4 py-16 text-white md:px-8 md:py-20"
     >
-        <div class="grid-pattern absolute inset-0 opacity-20"></div>
+        <div class="grid-pattern absolute inset-0 opacity-[3%]"></div>
         <div
             class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black/10"
         ></div>
@@ -14,4 +14,3 @@
         </div>
     </header>
 </template>
-
